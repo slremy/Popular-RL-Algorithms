2f5085508aa548f8b85ff7d8055df4c6579f6f8d
slremy <sekouremy@gmail.com>
2023-11-13 23:06:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
-
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
 
 setup(name='Popular_RL_Algorithms',
-      version='0.1',
+      version='0.1.1',
       description='PyTorch implementation of Soft Actor-Critic (SAC), Twin Delayed DDPG (TD3), Actor-Critic (AC/A2C), Proximal Policy Optimization (PPO), QT-Opt, PointNet..',
       author='quantumiracle',
       author_email='https://github.com/quantumiracle',
       url='https://github.com/quantumiracle/Popular-RL-Algorithms',
-      install_requires = ["gym", 
+      install_requires = ["gymnasium", 
           "torch",
           ],
       packages=find_packages(),
